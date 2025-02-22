cbeb83a7e1a3e969548e6b544a6808211b02fde3
alexandra.ciupitu <alexandra.ciupitu@stud.acs.upb.ro>
2025-02-22 13:04:25 +0200

    Added Dice project

---
+++ Day03/Dice/Assets/DiceSide.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DiceSide : MonoBehaviour
+{
+    bool onGround;
+    public int sideValue;
+
+    void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Ground")
+        {
+            onGround = false;
+        }
+    }
+
+    public bool OnGround()
+    {
+        return onGround;
+    }
+}
